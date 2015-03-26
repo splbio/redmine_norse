ef9d30f290742ed2aeec71faea9acc632ed233f4
Eric Davis <edavis@littlestreamsoftware.com>
2015-03-26 13:00:14 -0700

    [#7722] Add settings panel to configure the redirect url

--- init.rb
+++ init.rb
@@ -4,4 +4,11 @@ Redmine::Plugin.register :redmine_norse do
   description "Customizations to Redmine for Norse"
   version '1.0.0'
   author_url 'http://www.littlestreamsoftware.com'
+
+  settings({
+             :partial => 'settings/redmine_norse',
+             :default => {
+               'my_page_redirect' => nil
+             }
+           })
 end
